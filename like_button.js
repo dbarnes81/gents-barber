56e2bb5dd7a1301bc2ac252f89535dec1bca89d3
dbarnes81 <wondery81@gmail.com>
2019-05-10 14:05:08 -0400

    Haven

--- like_button.js
+++ like_button.js
@@ -1,5 +1,6 @@
 'use strict'; 
-const e = React.createElement;
+const reactElement = React.createElement;
+
 class LikeButton extends React.Component {
     constructor(props) {
         super(props);
@@ -10,14 +11,14 @@ class LikeButton extends React.Component {
                 return `You liked this.`; 
             }
 
-                return e( 'button', {
-                    onClick: () => this.setState({liked:true}) },
-                    'Like' ); 
-                    }
-                }
+                return  reactElement( 'button', {onClick: () => this.setState({liked:true}) },'Like' ); 
+                    
+                    
+    }
+}
                     
                 
 
 const domContainer = document.querySelector('#like_button_container');
-                    ReactDOM.render(e(LikeButton),
+                    ReactDOM.render(reactElement(LikeButton),
                     domContainer);
